1dab4e6e931b14d8dd42040aa9ae94c25437fc27
moslimor87 <33181914+moslimor87@users.noreply.github.com>
2022-10-01 21:19:42 +0300

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
-mkdir -p ~/ .streamlit/
+mkdir -p ~/.streamlit/
 
 echo "\
 [server]\n\
-port= process.env.PORT || 3000
+port= $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\" > ~/.streamlit/config.toml
